ee4acb6a8d270b698e23d7d0faa2a1500ad805d2
kai.reytsu <kai.reytsu@gmail.com>
2022-02-02 21:20:34 +0300

    Change del function

--- delete_elem.py
+++ delete_elem.py
@@ -7,14 +7,17 @@ from myrandom import rand_number, randint
 def delete_elem(list_num, num):
     '''Return delete elements from given list'''
     del_num = 0
-    for index, elem in enumerate(list_num):
-        if elem == num:
-            del list_num[index]
+    i = 0
+    while i < len(list_num):
+        if list_num[i] == num:
+            del list_num[i]
             del_num += 1
+        else:
+            i += 1
     return del_num
 
-list_of_num = rand_number(max_num=10, list_len=10)
-num_to_del = randint(1, 10)
+list_of_num = rand_number(max_num=5, list_len=5)
+num_to_del = randint(1, 5)
 print('Delete', num_to_del, 'from list', *list_of_num)
 print('Number of deleted elements =', delete_elem(list_of_num, num_to_del))
 print('New list', *list_of_num)
