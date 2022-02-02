aacbde48f4c53e6d506c56389b7d126185f42780
kai.reytsu <kai.reytsu@gmail.com>
2022-02-02 21:34:50 +0300

    Change list of two lists

--- list_of_two_lists.py
+++ list_of_two_lists.py
@@ -6,7 +6,7 @@ from myrandom import rand_number
 
 def two_lists(list_one, list_two):
     '''Return list of two lists'''
-    return set(list_one + list_two)
+    return list(set(list_one + list_two))
 
 list_one = rand_number(list_len = 5, max_num=10)
 list_two = rand_number(list_len = 5, max_num=10)
